77b4deee074b5a2504b426f990d61eb6fd76318a
Imre Nagi <imre.nagi2812@gmail.com>
2017-09-05 13:38:45 -0700

    Adding react router dependency

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import Home from './components/home/Home'
+import CandidateDetail from './components/candidatedetail/CandidateDetail'
 
 ReactDOM.render(
-  <Home />,
+  <CandidateDetail />,
   document.getElementById('app')
 )
