4a119e034caa7d53c73f604cfba59adfa2a98efa
Rizky Eko Putra <rizky.eko.putra@gmail.com>
2017-09-11 10:25:38 +0700

    Change ImageHeader to carousel uncontrol

--- src/components/imageheader/ImageHeader.js
+++ src/components/imageheader/ImageHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap'
+import { Button, Carousel,  } from 'react-bootstrap'
 
 import styles from './ImageHeader.css';
 
@@ -7,12 +7,29 @@ export default class ImageHeader extends React.Component {
 
   render() {
     return (
-      <div className="row header-bg">
-        <div className="header-content align-middle">
-          <h1>Rojak - Media Sentiment Analyzer!</h1>
-          <Button bsStyle='primary' bsSize='large'>Testing</Button>
-        </div>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+          <img src="/src/static/img/land-first.jpg"/>
+          <Carousel.Caption>
+            <h3>Rojak</h3>
+            <p>Media Sentiment Analyzer!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="/src/static/img/land-first.jpg"/>
+          <Carousel.Caption>
+            <h3>Rojak</h3>
+            <p>Media Sentiment Analyzer!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="/src/static/img/land-first.jpg"/>
+          <Carousel.Caption>
+            <h3>Rojak</h3>
+            <p>Media Sentiment Analyzer!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+  </Carousel>
     );
   }
 }
