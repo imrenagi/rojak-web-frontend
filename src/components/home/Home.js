b450a95e1352a35db92812bfa5546e9c509195a7
Alex Flasch <Flascher@users.noreply.github.com>
2017-10-03 15:57:45 -0500

    Feature/horizontal bar chart (#12)
    * Horizontal bar chart displaying correctly
    * Added tooltip to bars
    * Add d3 dependencies to project

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-import ImageHeader from './../imageheader/ImageHeader'
-import IndonesiaMap from './../maps/IndonesiaMap'
+import ImageHeader from './../imageheader/ImageHeader';
+import IndonesiaMap from './../maps/IndonesiaMap';
+import HorizontalBarChart from './../charts/HorizontalBarChart';
+
+// TODO: remove me after testing complete
+import testData from './../charts/test-data.json';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <IndonesiaMap />
+        <HorizontalBarChart chartData={testData}/>
       </div>
     );
   }
