859237cceb265ba86215f1a93c544e5bfc15223a
Eric Adamski <er.adamski@gmail.com>
2017-10-02 20:20:57 -0400

    First test, test setup and initial testing of a component.

--- src/components/videosection/VideoSection.js
+++ src/components/videosection/VideoSection.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
-import styles from './videosection.css'
+import './videosection.css';
 
 export default class VideoSection extends React.Component {
-
-  render() {
-    return (
-        <div className="wrapper style3 video-section">
-          <div className="row title">
-            <h1>About Rojak</h1>
-          </div>
-          <div className="row description">
-            <p>Watch this video to understand more about Rojak</p>
-          </div>
-          <div className="video-thumbnail">
-            <div className="container-video">
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/3tmd-ClpJxA"></iframe>
-              </div>
+    render() {
+        return (
+            <div className="wrapper style3 video-section">
+                <div className="row title">
+                    <h1>About Rojak</h1>
+                </div>
+                <div className="row description">
+                    <p>Watch this video to understand more about Rojak</p>
+                </div>
+                <div className="video-thumbnail">
+                    <div className="container-video">
+                        <div className="embed-responsive embed-responsive-16by9">
+                            <iframe
+                                className="embed-responsive-item"
+                                src="https://www.youtube.com/embed/3tmd-ClpJxA"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
