b491683c3ce6084193dba9439dc63d366796a5c9
Imre Nagi <imre.nagi2812@gmail.com>
2017-10-15 13:19:10 -0700

    Update navbar size

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,25 +1,22 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Image } from 'semantic-ui-react'
 
 export default class NavigationBar extends React.Component {
   render () {
     return (
-      <Menu>
-        <Menu.Item>
-          <a href='/'>Kawal Media ID</a>
+      <Menu stackable>
+        <Menu.Item >
+          <a href='/'>
+            <Image size='tiny' src='https://react.semantic-ui.com/logo.png' />
+          </a>
         </Menu.Item>
+
         <Menu.Menu position='right'>
-          <Menu.Item>
-            Kandidat
-          </Menu.Item>
-          <Menu.Item>
-            Media
-          </Menu.Item>
-          <Menu.Item>
+          <Menu.Item name='about-us'  size='medium'>
             About Us
           </Menu.Item>
         </Menu.Menu>
-      </Menu>
+     </Menu>
     )
   }
 }
