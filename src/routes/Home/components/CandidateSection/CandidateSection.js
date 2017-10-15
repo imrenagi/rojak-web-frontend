fc23c5dce967c276ab18ded60297c763e6619413
Imre Nagi <imre.nagi2812@gmail.com>
2017-10-15 13:26:17 -0700

    Add temp button in candidate section

--- src/routes/Home/components/CandidateSection/CandidateSection.js
+++ src/routes/Home/components/CandidateSection/CandidateSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './candidatesection.css'
+import { Route } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 export default class CandidateSection extends React.Component {
   constructor () {
@@ -17,16 +19,22 @@ export default class CandidateSection extends React.Component {
 
   render () {
     return (
-      <div className='wrapper style1 candidate'>
-        <div className='container-atas'>
-          <div className='row title'>
-            <h1>Kandidat</h1>
-          </div>
-          <div className='row description'>
-            <p>Kandidat yang turut meramaikan pemilu</p>
+      <Route render={({ history }) => (
+        <div className='wrapper style1 candidate'>
+          <div className='container-atas'>
+            <div className='row title'>
+              <h1>Kandidat</h1>
+            </div>
+            <div className='row description'>
+              <p>Kandidat yang turut meramaikan pemilu</p>
+            </div>
+            <Button fluid size='huge'
+              onClick={() => { history.push('/election/dkijakarta/candidate/1') }}>
+            Temp Candidate Detail Page</Button>
           </div>
         </div>
-      </div>
+      )} />
+
     )
   }
 }
